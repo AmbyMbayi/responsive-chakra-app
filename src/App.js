7d939c2702fc056e9812c8e7249fb5b808560c2d
Amby Mbayi <am@workpay.co.ke>
2021-06-21 08:26:06 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Box, Text } from '@chakra-ui/react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Box>
+			<Box border="solid 1px red" height="200px" backgroundColor={[ 'yellow', 'red', 'white', 'primary.500' ]}>
+				<Text fontSize={{ base: '20px', md: '40px', lg: '60px' }}>This is box</Text>
+			</Box>
+			<Box border="solid 1px black" height="200px">
+				<Text
+					fontSize={{ base: '20px', md: '40px', lg: '60px' }}
+					_hover={{
+						backgroundColor: [ 'red', 'green', 'purple', 'magenta' ]
+					}}>
+					Hover Me
+				</Text>
+			</Box>
+		</Box>
+	);
 }
 
 export default App;
